1b8fcc88aff719dae58147e3e42af9e45c13e089
KevinThePepper <kevin.shelley@pm.me>
2019-11-02 21:54:19 -0400

    Removed JWT token util
    Placed the util in the token endpoint

--- util/jwt_util.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const getJwtToken = (username, password) => {
-  const privateKey = fs.readFileSync('rsa');
-  return jwt.sign({username: username, password: password}, privateKey, {algorithm: 'RS256'});
-};
-
-export default getJwtToken;
