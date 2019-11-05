477b246a167e6596b5b92cdae2e651ca4b66aef2
KevinThePepper <kevin.shelley@pm.me>
2019-11-05 10:54:32 -0500

    Added middleware to verify JWT token

---
+++ middleware/jwt.middleware.js
@@ -0,0 +1,33 @@
+const JWTToken = require('../api/v2/token/Token');
+const InvalidJWTToken = require('../error/invalidJWTToken');
+
+function verifyJWT_MW(req, res, next) {
+  // check for endpoints that don't require jwt auth
+  let url = req.originalUrl;
+  console.log(url);
+  if (url === '/api/v2/login' || url === '/api/v2/token') {
+    next();
+    return
+  }
+
+  // get token from header
+  let header = req.headers['authorization'] || '';
+  let token = header.split(/\s+/).pop() || '';
+  let generator = new JWTToken();
+
+  // verify the token
+  try {
+    let verify = generator.verifyJwtToken(token);
+    console.log('Verified: ' + JSON.stringify(verify));
+
+    if (!verify['auth']) {
+      throw new InvalidJWTToken('Invalid JWT token');
+    } else {
+      next()
+    }
+  } catch (err) {
+    res.json(err)
+  }
+}
+
+module.exports = verifyJWT_MW;
