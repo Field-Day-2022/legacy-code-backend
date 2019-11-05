39213145f2d63c4f9521dcc0baa920eed6042bda
KevinThePepper <kevin.shelley@pm.me>
2019-11-05 10:52:09 -0500

    Added custom error to catch invalid JWT Token
    This may not be necessary since all of the errors so far have been caught by the jwt verification

---
+++ error/invalidJWTToken.js
@@ -0,0 +1,13 @@
+class InvalidJWTToken extends Error {
+  constructor (message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 401;
+  }
+
+  statusCode() {
+    return this.status
+  }
+}
+
+module.exports = InvalidJWTToken;
