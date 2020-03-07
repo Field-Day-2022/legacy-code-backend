3182d952585dce68e1ed8424daafc44be783c4aa
tbetz0324 <tarynbetz@gmail.com>
2020-03-07 05:25:53 -0500

    US184- document all "contributes_to" files

--- api/v2/contributes_to/ContributesTo.js
+++ api/v2/contributes_to/ContributesTo.js
@@ -1,20 +1,49 @@
+/*
+ * File: ContributesTo.js
+ * Version: 1.01
+ * Date: 2020-03-07
+ * Description: Main class for the contributes_to endpoint.
+ */
+
+/**
+ * Contains methods to GET, POST, and DELETE contributes_to entries
+ * to/from the database.
+ * @version 1.01
+ * @since 2020-03-07
+ */
 class ContributesToRepository {
   constructor(dao) {
     this.dao = dao;
   }
 
+  /**
+   * Returns data for all of the entries in the ContributesTo table.
+   * @return a collection of JSON objects representing the contributes_to
+   *            entries.
+   */
   getAll() {
     const sql = `SELECT * FROM ContributesTo`;
 
     return this.dao.all(sql, []);
   }
-
+  
+  /**
+   * Returns data for the given contributesTo relation.
+   * @param user_id id of the user entry will be associated with
+   * @param project_id id of the project entry will be associated with
+   * @return a JSON object representing the answerSetObject
+   */
   getOne(user_id, project_id) {
     const sql = `SELECT * FROM ContributesTo WHERE user_id = ? AND project_id = ?`;
 
     return this.dao.get(sql, [user_id, project_id]);
   }
-
+  
+  /**
+   * Saves a new ContributesTo entry to the database.
+   * @param contributesToObject a JSON object containing the contributesTo data
+   * @return the ID of the new contributesTo object, which indicates success
+   */
   post(contributesToObject) {
     const sql = `INSERT INTO ContributesTo (user_id, project_id, date_modified) VALUES (?, ?, ? )`;
 
@@ -25,6 +54,11 @@ class ContributesToRepository {
     ]);
   }
 
+  /**
+   * Updates an existing entry.
+   * @param contributesToObject a JSON object containing the updated data
+   * @return the ID of the entry, which indicates success
+   */
   update(contributesToObject) {
     const sql = `UPDATE ContributesTo SET user_id = ?, project_id = ?, date_modified = ? WHERE user_id = ?, AND project_id = ?`;
 
@@ -37,6 +71,12 @@ class ContributesToRepository {
     ]);
   }
 
+ /**
+   * Deletes the data entry from the database.
+   * @param user_id user id associated with the entry to be deleted
+   * @param project_id project id associated with the entry to be deleted
+   * @return the ID of the deleted entry, which indicates success
+   */
   delete(user_id, project_id) {
     const sql = `DELETE FROM ContributesTo WHERE user_id = ? AND project_id = ?`;
 
--- api/v2/contributes_to/contributes_to.router.js
+++ api/v2/contributes_to/contributes_to.router.js
@@ -1,16 +1,28 @@
+/*
+ * File: contributes_to.router.js
+ * Version: 1.01
+ * Date: 2020-03-07
+ * Description: Router for the contributes_to endpoint.
+ */
+
 const router = require('express').Router();
 const Controller = require('./contributes_to.controller');
 
+//create the controller and point it at the database
 const controller = new Controller(global.db);
 
+//set the user and project ids
 router.param('user_id', controller.user_id);
 router.param('project_id', controller.project_id);
 
+//if no queries or routes indicated, retrieve data for all entries
 router
   .route('/')
   .get(controller.getAll)
   .post(controller.post);
 
+//if user and project ids are present, retrieve the single matching data
+entry
 router
   .route('/:user_id/:project_id')
   .get(controller.getOne)
