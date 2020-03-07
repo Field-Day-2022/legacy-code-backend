122242235b623cd0d9d4457cd83f7c1758492cff
tbetz0324 <tarynbetz@gmail.com>
2020-03-07 16:12:38 -0500

    US194: Documentation for config files

--- config/development.js
+++ config/development.js
@@ -1,3 +1,12 @@
+/*
+ * File: development.js
+ * Version: 1.01
+ * Date: 2020-03-07
+ * Description: Sets configuration for running in development mode.
+ */
+
+//logging enabled
+//run out of port 8000
 module.exports = {
   logging: true,
   port: 8000,
